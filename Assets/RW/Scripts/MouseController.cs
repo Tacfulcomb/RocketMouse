8522a39f2a24cf0f1d4a8979cebb6ef52f43c33c
Tacfulcomb <crazydavebidie12@gmail.com>
2025-11-14 18:08:51 +0700

    How to Make a Game Like Jetpack Joyride in Unity 2D – Part 3
    Finishing up the game by setting up +animations
    +sounds
    +adding collectables
    +win/lose conditions
    +QOL features (ex: parallax BG)

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MouseController : MonoBehaviour
 {
+    public ParallaxScroll parallax;
+    public AudioSource jetpackAudio;
+    public AudioSource footstepsAudio;
+
+    public AudioClip coinCollectSound;
+
+    public Text coinsCollectedLabel;
+    public Button restartButton;
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
 
@@ -13,6 +23,9 @@ public class MouseController : MonoBehaviour
     public LayerMask groundCheckLayerMask;
     private Animator mouseAnimator;
     public ParticleSystem jetpack;
+    private bool isDead = false;
+    private uint coins = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,15 +42,28 @@ public class MouseController : MonoBehaviour
     void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
+        jetpackActive = jetpackActive && !isDead;
+
         if (jetpackActive)
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
-        Vector2 newVelocity = playerRigidbody.velocity;
-        newVelocity.x = forwardMovementSpeed;
-        playerRigidbody.velocity = newVelocity;
+
+        if (!isDead)
+        {
+            Vector2 newVelocity = playerRigidbody.velocity;
+            newVelocity.x = forwardMovementSpeed;
+            playerRigidbody.velocity = newVelocity;
+        }
+        if (isDead && isGrounded)
+        {
+            restartButton.gameObject.SetActive(true);
+        }
+
         UpdateGroundedStatus();
         AdjustJetpack(jetpackActive);
+        AdjustFootstepsAndJetpackSound(jetpackActive);
+        parallax.offset = transform.position.x;
     }
     void UpdateGroundedStatus()
     {
@@ -59,6 +85,55 @@ public class MouseController : MonoBehaviour
         {
             jetpackEmission.rateOverTime = 75.0f;
         }
+    }
+    void CollectCoin(Collider2D coinCollider)
+    {
+        coins++;
+        coinsCollectedLabel.text = coins.ToString();
+        Destroy(coinCollider.gameObject);
+        AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
+    }
+    void AdjustFootstepsAndJetpackSound(bool jetpackActive)
+    {
+        footstepsAudio.enabled = !isDead && isGrounded;
+        jetpackAudio.enabled = !isDead && !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackAudio.volume = 1.0f;
+        }
+        else
+        {
+            jetpackAudio.volume = 0.5f;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Coins"))
+        {
+            CollectCoin(collider);
+        }
+        else
+        {
+            HitByLaser(collider);
+        }
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+        if (!isDead)
+        {
+            AudioSource laserZap = laserCollider.gameObject.GetComponent<AudioSource>();
+            laserZap.Play();
+        }
+
+        isDead = true;
+        mouseAnimator.SetBool("isDead", true);
     }
 
 }
