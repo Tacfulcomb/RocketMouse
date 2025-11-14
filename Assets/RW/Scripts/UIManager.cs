e4ce0d809c729ec4e3ed6b901cb8e481b35249ed
Tacfulcomb <crazydavebidie12@gmail.com>
2025-11-15 00:43:09 +0700

    Introduction to Unity UI – Part 2
    Added Music Slider
    Added Buttons Effect/Animations to add to da feel!

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator dialog;
+    public Animator startButton;
+    public Animator settingsButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,18 @@ public class UIManager : MonoBehaviour
     public void StartGame() 
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+    public void OpenSettings() 
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+    public void CloseSettings() 
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 
 }
